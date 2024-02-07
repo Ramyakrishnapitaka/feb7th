771791826b8b35af0d41379845417cdde4a25f28
Ramyakrishnapitaka <154577647+Ramyakrishnapitaka@users.noreply.github.com>
2024-02-07 18:58:24 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,18 @@
+function Read(){
+    document.getElementById("hide1").style.display="block"
+}
+function Read2(){
+    document.getElementById("hide2").style.display="block"
+}
+function Read3(){
+    document.getElementById("hide3").style.display="block"
+}
+/*function Show1(){
+    document.getElementById("one").style.display="block"
+}
+function Showme(){
+    document.getElementById("two").style.display="block"
+}
+function Showin(){
+    document.getElementById("three").style.display="block"
+}*/
